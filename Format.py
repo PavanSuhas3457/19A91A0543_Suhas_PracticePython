7583d49627b2021fbe09150ac9f223508fc72f04
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-05-21 15:24:06 +0530

    Add files via upload

---
+++ Format.py
@@ -0,0 +1,18 @@
+num = int(input('Enter a Value:'))
+print('Octal Value of this value is %o'%(num))
+print('HexaDecimal Value of this lower case value is %x'%(num))
+print('HexaDecimal Value of this upper case value is %X'%(num))
+print('Character Value of this value is %c'%(num))
+print('Integer Value of this value is %d'%(num))
+string = input('Enter the String:')
+print('Hello {}! How are You'.format(string))
+'''
+Enter a Value:65
+Octal Value of this value is 101
+HexaDecimal Value of this lower case value is 41
+HexaDecimal Value of this upper case value is 41
+Character Value of this value is A
+Integer Value of this value is 65
+Enter the String:Suhas
+Hello Suhas! How are You
+'''
