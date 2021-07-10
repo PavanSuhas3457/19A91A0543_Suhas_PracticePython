444bf0f2d7fb0ec9781520ffcd97e303d21ed25f
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-07-10 15:14:54 +0530

    Add files via upload

---
+++ AccessList.py
@@ -0,0 +1,4 @@
+from ListOperations import *
+print(listSum([1,2,3,4,5,6]))
+print(SortList([65,33,12,66,22]))
+print(stringConversion('Hello Python!'))
---
+++ ListOperations.py
@@ -0,0 +1,11 @@
+def listSum(lst):
+    res = 0
+    for i in lst:
+        res += i
+    return res
+def SortList(lst):
+    lst.sort()
+    return lst
+def stringConversion(st):
+    lst = list(st)
+    return lst
