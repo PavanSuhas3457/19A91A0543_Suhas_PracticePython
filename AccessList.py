230ce4c123a2835e4b63bc062db7251f1270ca4d
PavanSuhas3457 <69577841+PavanSuhas3457@users.noreply.github.com>
2021-07-10 15:16:42 +0530

    Update AccessList.py

--- AccessList.py
+++ AccessList.py
@@ -2,3 +2,9 @@ from ListOperations import *
 print(listSum([1,2,3,4,5,6]))
 print(SortList([65,33,12,66,22]))
 print(stringConversion('Hello Python!'))
+'''
+Output:
+21
+[12, 22, 33, 65, 66]
+['H', 'e', 'l', 'l', 'o', ' ', 'P', 'y', 't', 'h', 'o', 'n', '!']
+'''
